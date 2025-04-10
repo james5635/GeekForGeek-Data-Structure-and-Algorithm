8669e588f366ccdfd2e6b2a5090871616e2f0490
james5635 <brorojame2029@gmail.com>
2025-04-10 16:54:54 +0700

    update

--- test/a.py
+++ test/a.py
@@ -1,4 +1,20 @@
+## Test 0
 from typing import Callable, Iterator, Tuple, Any
+
 a = [[1, "apple"], [2, "orange"], [3, "cherry"]]
 res: map[tuple[object, ...]] = map(tuple, a)
 print(type(res))
+
+## Test 1
+x = (i for i in range(10))
+
+
+def y(a):
+    return a
+
+
+k = (1, 2, 3)
+l = (x for x in range(10))
+n = [1, 2, 3]
+m = [i for i in range(2)]
+y(l for l in range(0,20))
